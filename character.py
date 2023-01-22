fd0ddd01f71b09402e72e11ed1a90c1f99ca708a
Ruben <ruben4a@gmail.com>
2023-01-22 22:14:03 +0000

    Adding character animations

--- character.py
+++ character.py
@@ -1,14 +1,15 @@
 import pygame
 import sys
+import os
 
 # Define some constants for the physics properties of our character
-GRAVITY = 0.6
+GRAVITY = 0.8
 JUMP_SPEED = 15
-MOVE_SPEED = 0.8
+MOVE_SPEED = 0.6
 
 class Character(pygame.sprite.Sprite):
 
-    def __init__(self, x, y, image):
+    def __init__(self, x, y):
         super().__init__()
         # Set the initial position of the character
         self.x = x
@@ -19,39 +20,105 @@ class Character(pygame.sprite.Sprite):
         # Set the initial acceleration of the character
         self.ax = 0
         self.ay = GRAVITY
-        # Set the image of the character
-        self.image = image
+        # Running frames
+        self.frames_run = []
+        for file_name in os.listdir('images/hero_sprites/run/'):
+            self.frames_run.append(pygame.image.load(f"images/hero_sprites/run/{file_name}")) 
+        # Jumping frames
+        self.frames_jump = []
+        for file_name in os.listdir('images/hero_sprites/jump/'):
+            self.frames_jump.append(pygame.image.load(f"images/hero_sprites/jump/{file_name}")) 
+        # Falling frames
+        self.frames_fall = []
+        for file_name in os.listdir('images/hero_sprites/fall/'):
+            self.frames_fall.append(pygame.image.load(f"images/hero_sprites/fall/{file_name}"))   
+        # Idle frames
+        self.frames_idle = []
+        for file_name in os.listdir('images/hero_sprites/idle/'):
+            self.frames_idle.append(pygame.image.load(f"images/hero_sprites/idle/{file_name}"))    
+        self.current_frame = 0
+        self.image = self.frames_run[self.current_frame]
+        self.frame_rate = 0.035
         # Get the width and height of the character's image
         self.rect = self.image.get_rect()
         self.width = self.rect.width
         self.height = self.rect.height
+        # Set animations
 
 
     def draw(self, surface):
         """Draw the character to the specified surface."""
         surface.blit(self.image, self.rect)
 
-    def update(self, WIDTH, HEIGHT):
+    def update(self, WIDTH, HEIGHT, time_passed_seconds):
         # Update the velocity based on the acceleration
         self.vx += self.ax
         self.vy += self.ay
         # Update the position based on the velocity
         self.x += self.vx
+        if self.vx > 0:
+            going_right = True
+        else:
+            going_right = False
         self.y += self.vy
         # Apply friction to the character's movement
-        self.vx *= 0.9
+        self.vx *= 0.8
         # Check for boundary collisions and adjust the character's position if necessary
-        if self.x < 0:
-            self.x = 0
-        elif self.x > WIDTH - self.width:
-            self.x = WIDTH - self.width
+        max_out_window = 30
+        if self.x < -max_out_window:
+            self.x = WIDTH - self.width + max_out_window
+        elif self.x > WIDTH - self.width + max_out_window:
+            self.x = -max_out_window
         if self.y < 0:
             self.y = 0
         elif self.y > HEIGHT - self.height:
             self.y = HEIGHT - self.height
             self.vy = 0
         # Update the rect attribute based on the new position
-        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+        keys = pygame.key.get_pressed()
+
+        if self.vy < 0:
+            self.current_frame += time_passed_seconds / self.frame_rate
+            if self.current_frame >= len(self.frames_jump):
+                self.current_frame = 0
+            if self.vx >= 0:
+                self.image = self.frames_jump[int(self.current_frame)]
+            else:
+                self.image = pygame.transform.flip(self.frames_jump[int(self.current_frame)], True, False)
+
+        elif self.vy > 0:
+            self.current_frame += time_passed_seconds / self.frame_rate
+            if self.current_frame >= len(self.frames_fall):
+                self.current_frame = 0
+            if self.vx >= 0:
+                self.image = self.frames_fall[int(self.current_frame)]
+            else:
+                self.image = pygame.transform.flip(self.frames_fall[int(self.current_frame)], True, False)
+
+        elif keys[pygame.K_RIGHT]:
+            self.current_frame += time_passed_seconds / self.frame_rate
+            if self.current_frame >= len(self.frames_run):
+                self.current_frame = 0
+            self.image = self.frames_run[int(self.current_frame)]
+
+        elif keys[pygame.K_LEFT]:
+            self.current_frame += time_passed_seconds / self.frame_rate
+            if self.current_frame >= len(self.frames_run):
+                self.current_frame = 0
+            self.image = pygame.transform.flip(self.frames_run[int(self.current_frame)], True, False)
+
+        else:
+            self.current_frame += time_passed_seconds / self.frame_rate
+            if self.current_frame >= len(self.frames_idle):
+                self.current_frame = 0
+            if going_right:
+                self.image = self.frames_idle[int(self.current_frame)]
+            else:
+                self.image = pygame.transform.flip(self.frames_idle[int(self.current_frame)], True, False)
+
+        # Updating rect
+        self.rect.x = self.x
+        self.rect.y = self.y
 
     def handle_key_events(self):
         for event in pygame.event.get():
@@ -59,7 +126,7 @@ class Character(pygame.sprite.Sprite):
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
+                if event.key == pygame.K_SPACE and  self.vy == 0:
                     self.jump()
                 elif event.key == pygame.K_LEFT:
                     self.ax = -MOVE_SPEED
@@ -103,4 +170,7 @@ class Character(pygame.sprite.Sprite):
                     # The character is moving up, so the collision is on the bottom of the character
                     self.y += overlap_y
                     self.vy = 0
+            # Updating rect
+            self.rect.x = self.x
+            self.rect.y = self.y
 
--- main.py
+++ main.py
@@ -19,19 +19,10 @@ screen = pygame.display.set_mode(window_size)
 # Set the title of the window
 pygame.display.set_caption('Pygame Game')
 
-# Set the character image
-# character_image = pygame.image.load('.\character_images\SpriteSheet.png')
-character_image = pygame.image.load('character.png')
-width = character_image.get_rect().width
-height = character_image.get_rect().height
-character_image = pygame.transform.scale(character_image, (width/3.5, height/3.5))
-character = Character(100, 100, character_image)
-
-# Get the character image size
-character_size = character_image.get_size()
+character = Character(100, 100)
 
 # Set the character starting position
-character_position = [0, 600 - height/3]
+character_position = [0, 600 - HEIGHT/3]
 
 # Set the character speed (pixels per frame)
 character_speed = 5
@@ -45,11 +36,13 @@ while True:
     # Handle events
     character.handle_key_events()
 
+    time_passed = clock.tick(120)
+    time_passed_seconds = time_passed / 1000.0
     # Update the character's position
-    character.update(WIDTH, HEIGHT)
+    character.update(WIDTH, HEIGHT, time_passed_seconds)
 
     # Check for collisions with the platform
-    plat = Platform(WIDTH/2, HEIGHT/1.3, 100, 20)
+    plat = Platform(WIDTH/2, HEIGHT/1.2, 100, 20)
     character.collide_with_platform(plat)
 
     # Clear the screen
@@ -57,7 +50,10 @@ while True:
 
     pygame.draw.rect(screen, (255, 0, 0), plat)
     # debug([int(x) for x in character_position])
-    # debug(f'{str(int(character.x))}, {str(int(character.y))}')
+    debug(f'{str(int(character.x))}, {str(int(character.y))}', 30)
+    debug(character.rect, 60)
+    debug(character.vy, 90)
+    debug(f'{str(int(plat.x))}, {str(int(plat.y))}')
     # debug(character.vy)
     # debug(pygame.Rect.colliderect(character.rect, plat.rect), 30)
 
